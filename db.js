3bfd0dd3b16f88476321891c1ee09776f8d44530
ARJUN-NAGAR <145248689+ARJUN-NAGAR@users.noreply.github.com>
2025-06-28 15:19:35 +0530

    Add files via upload
    Signed-off-by: ARJUN-NAGAR <145248689+ARJUN-NAGAR@users.noreply.github.com>

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
-import mongoose from 'mongoose';
+/*const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/nyayasaathi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected successfully.");
+}).catch(err => {
+  console.error("MongoDB connection error:", err);
+});*/
+import mongoose from 'mongoose';
 mongoose.connect('mongodb://localhost:27017/nyayasaathi', {
   useNewUrlParser: true,
   useUnifiedTopology: true
